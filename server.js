03385bf37858a9aab2633f1c6c836ef3357e7ccf
Artiely <1119696785@qq.com>
2018-09-08 16:06:55 +0800

    no message

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.use(express.static(__dirname + '/dist'))
 // }
 // var exampleProxy = proxy(options) //开启代理功能，并加载配置
 // app.use('/ns-index', exampleProxy) //对地址为’/‘的请求全部转发
-app.listen(3111)
+app.listen(3101)
