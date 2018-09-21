28241d9d27e84c0cbfa4ea4df1ca3de91cc17a09
Artiely <1119696785@qq.com>
2018-09-21 20:15:34 +0800

    系统图标

--- src/views/sys/sysMenu.vue
+++ src/views/sys/sysMenu.vue
@@ -4,7 +4,7 @@
       <a-tree showLine :defaultExpandedKeys="['0-0-0']" @select="onSelect">
         <a-tree-node :key="item.menuId+''" v-for="item in data">
           <span slot="title" style="color: #1890ff">{{item.name}}</span>
-          <a-tree-node :key="sub.menuId+''" v-for="sub in item.children">
+          <a-tree-node :key="sub.menuId+''" v-for="sub in item.children" @dblclick.native="edit(sub)">
             <div class="sub-menu-tree" slot="title">{{sub.name}}
               <a-icon class="pull-right tree-icon-right" type="edit" />
             </div>
@@ -16,20 +16,69 @@
     <pre>
       <!-- {{data}} -->
     </pre>
+    <a-modal title="Basic Modal" v-model="visible" @ok="handleOk">
+      <a-form :autoFormCreate="(form)=>{this.form = form}">
+        <a-form-item :labelCol="formItemLayout.labelCol" :wrapperCol="formItemLayout.wrapperCol" label='类型' fieldDecoratorId="type" :fieldDecoratorOptions="{rules: [{ required: true}]}">
+          <a-radio-group :options="options" :defaultValue="value" />
+        </a-form-item>
+        <a-form-item :labelCol="formItemLayout.labelCol" :wrapperCol="formItemLayout.wrapperCol" label='目录名称' fieldDecoratorId="name" :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入目录名称'}]}">
+          <a-input placeholder='目录名称' type="text" />
+        </a-form-item>
+        <a-form-item :labelCol="formItemLayout.labelCol" :wrapperCol="formItemLayout.wrapperCol" label='排序号' fieldDecoratorId="newPassword" >
+          <a-input-number :min="1" :max="100" />
+        </a-form-item>
+        <a-form-item :labelCol="formItemLayout.labelCol" :wrapperCol="formItemLayout.wrapperCol" label='菜单图标' fieldDecoratorId="newPassword" >
+          <a-input placeholder='菜单图标' type="text" @click="visible2=true"/>
+        </a-form-item>
+      </a-form>
+    </a-modal>
+     <a-modal title="请选择图标" v-model="visible2" @ok="handleOk">
+       <sys-icon></sys-icon>
+    </a-modal>
   </div>
 </template>
 
 <script>
 import { treeDataTranslate } from '@/utils'
+import sysIcon from './sysIcon'
+const formItemLayout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 18 }
+}
+const formTailLayout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 18, offset: 6 }
+}
+const options = [
+  { label: '目录', value: '1' },
+  { label: '菜单', value: '2' },
+  { label: '按钮', value: '3' }
+]
 export default {
+  components: {
+    sysIcon
+  },
   data() {
     return {
-      data: []
+      data: [],
+      formItemLayout,
+      formTailLayout,
+      options,
+      value: '2',
+      visible: false,
+      visible2: false
     }
   },
   methods: {
     onSelect(selectedKeys, info) {
       console.log('selected', selectedKeys, info)
+    },
+    edit() {
+      this.visible = true
+      // alert(1)
+    },
+    handleOk(e) {
+      this.visible = false
     },
     getData() {
       this.$store.dispatch('getMenuList').then(result => {
